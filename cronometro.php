6b6ca7c2a198a072e440e9e390083d976e0bd97d
UO288047 <UO288047@uniovi.es>
2025-11-26 14:56:18 +0100

    trabajo clase 10

---
+++ cronometro.php
@@ -0,0 +1,30 @@
+class Cronometro{
+
+    private $tiempo;
+    private $inicio;
+
+    public function __construct(){
+        $this->tiempo = 0;
+    }
+
+
+    public function arrancar(){
+        $this->inicio = microtime(true);
+    }
+
+
+    public function parar(){
+       $fin =  microtime(true);
+       $this->tiempo = $fin - $this->inicio;
+    }
+
+
+    public function mostrar(){
+        $minutosTranscurridos = floor($this->tiempo / 60);
+        $segundosYdecimasTranscurridos = $this->tiempo - ($minutosTranscurridos * 60);
+        //Revisar si se usa printf o echo
+        printf("%02d:%04.1f", $minutosTranscurridos, $segundosYdecimasTranscurridos);
+    }
+
+
+}
